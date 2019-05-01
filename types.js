bf12c380d1a51f5430081a8c3fc7ddfb44baa3d3
mizhou0817 <mizhou0817@gmail.com>
2019-04-30 21:21:42 -0400

    done with chain

--- types.js
+++ types.js
@@ -36,6 +36,7 @@ d3.csv("pokemon_species.csv", function(speciesData) {
 		for (let t in evolvePokemonData) {
 			if (evolvePokemonData[t].type === thisType) {
 				evolvePokemonData[t].pokemons.push({
+					pokemonName: speciesData[poke].identifier,
 					id: speciesData[poke].id,
 					primitive: isPrimitive,
 					evolvesFrom: speciesData[poke].evolves_from_species_id
@@ -48,7 +49,7 @@ d3.csv("pokemon_species.csv", function(speciesData) {
 
 
 
-	//console.log(evolvePokemonData[0].pokemons);
+	console.log(evolvePokemonData);
 
 	for (let typeNum in types) {
 		var myImage = new Image(50, 19);
@@ -82,6 +83,83 @@ d3.csv("pokemon_species.csv", function(speciesData) {
 							myImage.src = "pokemon/" + thesePokemons[poke].id + ".png";
 							myImage.id = "thisImg";
 							myImage.className = types[typeNum];
+
+
+							myImage.onclick = function () {
+								const toRemove = document.getElementsByClassName('evolutionPokemon')
+									while (toRemove.length > 0) {
+										toRemove[0].parentNode.removeChild(toRemove[0])
+									}
+
+
+
+
+								var myImage = new Image(64, 64);
+								myImage.src = "pokemon/" + thesePokemons[poke].id + ".png";
+								myImage.id = thesePokemons[poke].id;
+								myImage.className = 'evolutionPokemon'
+
+								document.getElementById("evolution_chain").appendChild(myImage);
+
+
+
+
+								isEvolve = traceEvolution(thesePokemons[poke].id);
+
+
+								for (var evolveDirection in isEvolve[1]) {
+									//console.log(isEvolve[1][evolveDirection]);
+									isEvolve2 = traceEvolution(isEvolve[1][evolveDirection]);
+								}
+
+
+
+								
+
+
+								function traceEvolution (id) {
+
+									var thesePokemons2 = [];
+
+									var continueTrace = false;
+									var continueId = [];
+									for (let t2 in evolvePokemonData) {
+										thesePokemons2 = evolvePokemonData[t2].pokemons;
+
+										
+										for (let poke2 in thesePokemons2) {
+											if (thesePokemons2[poke2].evolvesFrom === id) {
+												continueId.push(thesePokemons2[poke2].id)
+
+												var myImage = new Image(64, 64);
+												myImage.src = "pokemon/" + thesePokemons2[poke2].id + ".png";
+												myImage.id = thesePokemons2[poke2].id;
+												myImage.className = 'evolutionPokemon'
+
+												document.getElementById("evolution_chain").appendChild(myImage);
+
+
+
+												continueTrace = true;
+
+											}
+
+										}
+
+
+									}
+
+									return [continueTrace, continueId];
+
+								}
+
+
+
+
+
+
+
+							}
 
 							document.getElementById("primitive_pokemons").appendChild(myImage);
 						}
